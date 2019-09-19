98299e12a848a21327af40703e9281d98f521e08
Lea <452224689@qq.com>
2019-09-19 19:33:41 +0800

    feat storage

--- DataStore.js
+++ DataStore.js
@@ -1,20 +1,15 @@
-class DataStore {
-    static getInstance() {
-        if (!DataStore.instance) {
-            DataStore.instance = new DataStore;
-        }
-        return DataStore.instance;
-    }
-
-    constructor() {
-        this.map = new Map();
-        this.session = window.sessionStorage;
-        this.storage = window.localStorage;
-
-        this.session.__proto__ = Object.assign(window.sessionStorage.__proto__, storageAPI);
-        this.storage.__proto__ = Object.assign(window.sessionStorage.__proto__, storageAPI);
-    }
-}
+/*  [数据储存器]
+**  通过本地或内存储存实现,封装sessionStorage、localStorage和Map,
+**  @api
+**  set(key, val, {exp})    通过key, val储存数据,如果对象是localStorage可以设置exp(type:number)过期时间 单位/s
+**  get(key)    读取key对应的键值
+**  has(key)    返回一个布尔值，表示某个键是否存在
+**  delete(key) 删除某个键
+**  clear()     清除所有成员
+**  keys()      获取所有的键名遍历器
+**  values()    返回所有键值遍历器
+**  entries()   返回所有成员遍历器
+*/
 
 const storageAPI = {
     set(key, val, exp) {
@@ -26,12 +21,12 @@ const storageAPI = {
             let curTime = new Date().getTime();
             let expMS = exp * 1000;
 
-            var serializeVal = JSON.stringify({data: val, time: curTime, exp: expMS});
+            let serializeVal = JSON.stringify({data: val, time: curTime, exp: expMS});
 
             this.setItem(key, serializeVal);
         } else {
-            var serializeVal = JSON.stringify(val);
-            this.setItem(key, serializeVal)
+            let serializeVal = JSON.stringify(val);
+            this.setItem(key, serializeVal);
         }
 
         return val
@@ -72,30 +67,48 @@ const storageAPI = {
 
     keys() {
         const keys = [];
-        for (let i = 0 ; i > this.length; i++) {
+        for (let i = 0 ; i < this.length; i++) {
             keys.push(this.key(i))
-        };
+        }
         return keys
     },
 
     values() {
         const values = [];
-        for (let i = 0; i > this.length; i++) {
+        for (let i = 0; i < this.length; i++) {
             const key = this.key(i);
             const val = this.get(key);
             values.push(val);
-        };
+        }
         return values
     },
 
     entries() {
         const entries = [];
-        for (var i = 0; i > this.length; i++) {
+        for (var i = 0; i < this.length; i++) {
             const key = this.key(i);
             const value = this.get(key);
             entries.push({key, value});
-        };
+        }
         return entries
+    }
+}
+
+class DataStore {
+    static getInstance() {
+        if (!DataStore.instance) {
+            DataStore.instance = new DataStore;
+        }
+        return DataStore.instance;
+    }
+
+    constructor() {
+        this.map = new Map();
+        this.session = window.sessionStorage;
+        this.storage = window.localStorage;
+
+        Object.setPrototypeOf(this.session, Object.assign(Object.getPrototypeOf(this.session), storageAPI));
+        Object.setPrototypeOf(this.storage, Object.assign(Object.getPrototypeOf(this.storage), storageAPI));
     }
 }
 
